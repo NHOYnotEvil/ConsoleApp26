4882fee7f80377cdba69af931175d4e93a999b6f
NHOY <NHOY@2210DR000919>
2023-05-17 11:40:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Пистолетов";
+            string surname = "Володя";
+            string correction;
+
+            Console.WriteLine("Ваше имя - " + name + ". Ваша фамилия - " + surname + ".");
+
+            correction = name;
+            name = surname;
+            surname = correction;
+
+            Console.WriteLine("Ваше имя - " + name + ". Ваша фамилия - " + surname + ".");
+        }
+    }
+}
